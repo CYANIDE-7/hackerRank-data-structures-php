955e524d3917ef742f7e50c7305fb306aa2d9e6d
FAUZAN KHOT <fauzan.khot09@gmail.com>
2022-05-09 18:54:10 +0530

    Create Sales by Match.php

---
+++ Sales by Match.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+function sockMerchant($n, $ar) {
+    $count=0;
+    $arr= array_count_values($ar);
+    foreach($arr as $color => $amount)
+    {
+        $count=$count+  floor($amount/2);
+    }
+    
+    return $count;
+    
+    
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$n = intval(trim(fgets(STDIN)));
+
+$ar_temp = rtrim(fgets(STDIN));
+
+$ar = array_map('intval', preg_split('/ /', $ar_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$result = sockMerchant($n, $ar);
+
+fwrite($fptr, $result . "\n");
+
+fclose($fptr);
